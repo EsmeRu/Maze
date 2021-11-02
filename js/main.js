bda81cd361faa1c556ca5693bf7ac586ee2d7e5e
EsmeRubin <rubin.esmef@gmail.com>
2021-11-02 04:46:28 -0700

    detalles

--- js/main.js
+++ js/main.js
@@ -1,45 +1,33 @@
 var canvas = null;
 var ctx = null;
-var color = "rgba(255,0,0,0.5)";
-
 var lastPress;
 var dir = 68,
   speed = 4,
-  score = 0,
   data = 0;
-var isPressed = false;
-var pantalla = document.getElementById("screen");
-
 var globalX = 20,
   globalY = 30;
-
 var player = null;
 var walls = new Array(),
   stone = new Array();
-
 var iPlayer = new Image(),
   iWallsV = new Image(),
   iWallsH = new Image(),
   iStone = new Image(),
   fondo = new Image();
-
 var newTime = false,
   acumularTime = 0;
-
-var sounds = new Array();
+var time = new Array(),
+  sounds = new Array(),
+  dirPlayer = new Array();
 for (var x = 0; x <= 6; x++) sounds[x] = new Audio();
-
-var dirPlayer = new Array();
 for (var x = 0; x <= 39; x++) dirPlayer[x] = new Image();
-
 var walk = null;
+var tamImage = 20;
 
 //Se encarga de pintar todo en pantalla
 function paint(ctx) {
   ctx.drawImage(fondo, 0, 0);
-
   for (var i = walls.length - 1; i >= 0; i--) {
-    // walls[i].fill(ctx);
     var wX = walls[i].x;
     var wY = walls[i].y;
 
@@ -47,31 +35,27 @@ function paint(ctx) {
       ctx.drawImage(iWallsH, walls[i].x - 3, wY);
       wY += 25;
     }
-
     while (walls[i].w + walls[i].x >= wX + iWallsV.width) {
       if (
         (walls[i].x == 0 && walls[i].y == 0) ||
         (walls[i].x == 0 && walls[i].y == 505)
-      ) {
+      )
         ctx.drawImage(iWallsH, wX, walls[i].y - 20);
-      } else ctx.drawImage(iWallsV, wX + 5, walls[i].y);
+      else ctx.drawImage(iWallsV, wX + 5, walls[i].y);
       wX += 25;
     }
   }
   ctx.drawImage(walk, player.x, player.y - 10);
 
-  for (var i = stone.length - 1; i >= 0; i--) {
-    ctx.drawImage(iStone, stone[i].x + 10, stone[i].y - 10);
-  }
+  for (var i = stone.length - 1; i >= 0; i--)
+    ctx.drawImage(iStone, stone[i].x + 5, stone[i].y - 8);
 
-  ctx.fillStyle = "black";
-  ctx.font = "20px Arial";
-  ctx.fillText("SCORE: " + score, 10, 20);
+  ctx.fillStyle = "#fff";
+  ctx.font = "20px Courier New";
+  ctx.fillText("TIEMPO: " + time[2] + ":" + time[1] + ":" + time[0], 500, 20);
 }
-
 //Movimientos de la pintura
 function update() {
-  //Sonido de fondo
   sounds[2].oncanplaythrough = (event) => {
     sounds[2].loop = true;
     var playedPromise = sounds[2].play();
@@ -79,12 +63,14 @@ function update() {
       playedPromise.catch((e) => {
         if (e.name === "NotAllowedError" || e.name === "NotSupportedError") {
           console.log(e.name);
+          run();
         }
       });
     }
   };
+
   for (var i = walls.length - 1; i >= 0; i--) {
-    if (player.touch(walls[i])) {
+    if (player.touch(walls[i]) || player.x < -2 || player.x > 700) {
       if (dir == "arriba") player.y += speed;
       if (dir == "abajo") player.y -= speed;
       if (dir == "derecha") player.x -= speed;
@@ -100,22 +86,20 @@ function update() {
     }
   }
 }
-
 //Manda pintar y actualizar los frames
 function start() {
   window.requestAnimationFrame(start);
   update();
   paint(ctx);
-  startTime();
 }
-
 //inicializa las variables necesarias
 function run() {
+  startTime();
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
   walk = dirPlayer[0];
 
-  player = new Cuadrado(globalX, globalY, 20, 20, color);
+  player = new Cuadrado(globalX, globalY, 20, 20);
 
   for (var x = 0; x <= 39; x++) {
     dirPlayer[x].src = "assets/player/" + (x + 1) + ".png";
@@ -131,15 +115,19 @@ function run() {
   iWallsH.src = "assets/baya2.png";
   iWallsV.src = "assets/arbusto1.png";
 
-  var tamImage = 20;
-  //Bordes
+  border();
+  verticalWall();
+  horizontalWall();
+  start();
+}
+const border = () => {
   walls.push(new Cuadrado(684, 0, tamImage, 400));
   walls.push(new Cuadrado(0, 0, 700, tamImage));
   walls.push(new Cuadrado(3, 65, tamImage, 170));
   walls.push(new Cuadrado(3, 290, tamImage, 225));
   walls.push(new Cuadrado(0, 505, 700, tamImage));
-
-  //barreras verticales
+};
+const verticalWall = () => {
   var x = 30,
     w = 250; //Primera columna vertical
   for (var y = 50; y <= 450; y += 50) {
@@ -164,43 +152,59 @@ function run() {
     walls.push(new Cuadrado(x, y, w, tamImage, "grey"));
     x = y == 50 ? 525 : 620;
   }
-
-  // stone.push(new Cuadrado(260, 210, 33, 30));
-
-  //barreras horizontales
-  // walls.push(new Cuadrado(390, 50, tamImage, 50, "grey"));
-  // walls.push(new Cuadrado(370, 150, tamImage, 75, "grey"));
-
-  // walls.push(new Cuadrado(240, 255, tamImage, 100, "grey"));
-  // walls.push(new Cuadrado(175, 350, tamImage, 100, "grey"));
-  // walls.push(new Cuadrado(275, 355, tamImage, 100, "grey"));
-  // walls.push(new Cuadrado(225, 400, tamImage, 100, "grey"));
-  // walls.push(new Cuadrado(100, 455, tamImage, 50, "grey"));
-
-  // stone.push(new Cuadrado(480, 20, 50, 50));
-  // stone.push(new Cuadrado(600, 60, 50, 50));
-
-  // stone.push(new Cuadrado(470, 150, 50, 50, "grey"));
-  // stone.push(new Cuadrado(575, 200, 50, 50, "grey"));
-  // stone.push(new Cuadrado(380, 250, 50, 50, "grey"));
-  // stone.push(new Cuadrado(450, 300, 50, 50, "grey"));
-  // stone.push(new Cuadrado(350, 350, 50, 50, "grey"));
-  // stone.push(new Cuadrado(490, 400, 50, 50, "grey"));
-
-  start();
-}
-
-function Cuadrado(x, y, w, h, color) {
+};
+const horizontalWall = () => {
+  const stonesXY = [
+    [390, 20],
+    [600, 45],
+    [555, 70],
+    [430, 70],
+    [40, 70],
+    [127, 80],
+    [203, 50],
+    [535, 120],
+    [330, 120],
+    [260, 145],
+    [25, 195],
+    [430, 170],
+    [330, 170],
+    [620, 195],
+    [485, 220],
+    [170, 220],
+    [270, 220],
+    [545, 237],
+    [640, 267],
+    [190, 267],
+    [400, 267],
+    [25, 300],
+    [540, 320],
+    [300, 320],
+    [620, 345],
+    [200, 370],
+    [95, 320],
+    [25, 395],
+    [455, 365],
+    [400, 420],
+    [110, 450],
+    [547, 450],
+    [590, 475],
+    [615, 475],
+    [640, 475],
+    [665, 475],
+  ];
+  for (var x = 0; x <= stonesXY.length - 1; x++) {
+    stone.push(new Cuadrado(stonesXY[x][0], stonesXY[x][1], tamImage, 40));
+  }
+};
+function Cuadrado(x, y, w, h) {
   //atributos
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
-  this.color = color;
 
   //funciones
   this.fill = function (ctx) {
-    ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.w, this.h);
   };
 
@@ -224,8 +228,10 @@ function winner(salida) {
   } else {
     gif = "url('./assets/win.gif')";
   }
+  stop();
   Swal.fire({
     title: `<h1 style="color: #fff; font-size: 2.5rem;">¡Felicidades! \n Has llegado a la salida ${salida}</h1>`,
+    html: `<p style="color: #fff;">Terminaste el juego en ${time[2]}:${time[1]}:${time[0]}</p>`,
     background: "transparent",
     confirmButtonText: "Aceptar",
     confirmButtonColor: "#2E6D1D",
@@ -236,7 +242,9 @@ function winner(salida) {
       no-repeat
     `,
   }).then((value) => {
-    run();
+    if (value) {
+      run();
+    }
   });
 } //Alerta de ganador
 function warning() {
@@ -250,6 +258,8 @@ function warning() {
       center center
       no-repeat
     `,
+    showConfirmButton: false,
+    timer: 1300,
   });
 } //Alerta de camino equivocado
 function startTime() {
@@ -264,13 +274,12 @@ function timer() {
   acumularTime = timeActual - timeInicial;
   acumularTime2 = new Date();
   acumularTime2.setTime(acumularTime);
-  ss = acumularTime2.getSeconds();
-  mm = acumularTime2.getMinutes();
-  hh = acumularTime2.getHours() - 18;
-  if (ss < 10) ss = "0" + ss;
-  if (mm < 10) mm = "0" + mm;
-  if (hh < 10) hh = "0" + hh;
-  pantalla.innerHTML = hh + " : " + mm + " : " + ss;
+  time[0] = acumularTime2.getSeconds();
+  time[1] = acumularTime2.getMinutes();
+  time[2] = acumularTime2.getHours() - 18;
+  if (time[0] < 10) time[0] = "0" + time[0];
+  if (time[1] < 10) time[1] = "0" + time[1];
+  if (time[2] < 10) time[2] = "0" + time[2];
 } //Contador de tiempo
 function stop() {
   if (newTime == true) {
@@ -284,9 +293,8 @@ function reset() {
     newTime = false;
   }
   acumularTime = 0;
-  pantalla.innerHTML = "00 : 00 : 00";
+  time[0] = time[1] = time[2] = "00";
 } //reset del tiempo
-
 document.addEventListener(
   //Controles
   "keydown",
@@ -297,19 +305,26 @@ document.addEventListener(
       dir = "arriba";
       player.y -= speed;
       data = 20;
-    } else if (lastPress == 40) {
+    }
+    if (lastPress == 40) {
       dir = "abajo";
       player.y += speed;
       data = 0;
-    } else if (lastPress == 39) {
+    }
+    if (lastPress == 39) {
       dir = "derecha";
       player.x += speed;
       data = 30;
-    } else if (lastPress == 37) {
+    }
+    if (lastPress == 37) {
       dir = "izquierda";
       player.x -= speed;
       data = 10;
-    } else console.log("presiona otra tecla");
+    }
+    if (lastPress == 32) {
+      if (sounds[2].muted == true) sounds[2].muted = false;
+      else sounds[2].muted = true;
+    }
 
     if (data != null) {
       walk =
@@ -317,7 +332,7 @@ document.addEventListener(
           ? dirPlayer[dirPlayer.indexOf(walk) + 1]
           : dirPlayer[data];
     }
-    if (player.x < 0 && player.y < 100) {
+    if (player.x < 0 && player.y < 70) {
       warning();
     }
     if (player.x > 700) {
@@ -329,7 +344,6 @@ document.addEventListener(
   },
   false
 );
-
 window.requestAnimationFrame = (function () {
   return (
     window.requestAnimationFrame ||
@@ -340,7 +354,4 @@ window.requestAnimationFrame = (function () {
     }
   );
 })();
-
-window.onload = () => {};
-
 window.addEventListener("load", run, false);
